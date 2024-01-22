edb6df53e5789ea70f4fbdf7476f162ab50a2936
thegypsyboyo <lewisonyango9@gmail.com>
2024-01-22 17:56:42 +0300

    made a basic redux reducer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,19 @@
 // import "../styles/globals.css";
 import styles from "../styles/globals.scss"
+import { Provider } from "react-redux";
+import store from "../../store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component  {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
